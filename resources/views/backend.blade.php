59a2fd7d74a0834be72f5de2abc4e5323ea03767
FifiYasmine Salhi <fifisalhi633@gmail.com>
2025-08-08 18:14:10 +0200

    Initial commit

---
+++ resources/views/backend.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Backend Dashboard</title>
+   
+</head>
+<body>
+            <h2 class="text-xl mb-4">Welcome to the Backend Dashboard</h2>
+            <p>This is a simple backend dashboard built with Laravel.</p>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+</head>
+<body>
+    
+    <style>
+        body {
+            background-color: #e19fccff;
+         
+        }
+        .navbar .nav-link {
+    font-size: 28px;
+  }
+  .navbar .navbar-brand {
+    font-size: 28px;
+  } 
+   
+  h1 { margin: 0;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #c69fb4ff;
+            font-family: 'Arial', sans-serif
+             }
+       .background-image {
+    background-image: url('img/mac.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 90vh;
+}
+
+ 
+        </style>
+      
+    
+  
+  
+    <nav class="navbar navbar-expand-lg "  style="background-color: #b497acff;" data-bs-theme="light">
+  <div class="container-fluid">
+    <a class="navbar-brand fw-bold" href="#">MY <i class="fa-solid fa-heart text-danger"></i> COSMETICS</a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Categories
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Makeup</a></li>
+            <li><a class="dropdown-item" href="#">Skincare</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Perfume</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Help</a>
+        </li>
+      </ul>
+      <form class="d-flex w-90" role="register">
+        
+       <button type="button" class="btn btn-light btn-lg me-2">register</button>
+         <button type="button" class="btn btn-light btn-lg">Login</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+ 
+  <div class="background-image">
+   
+</div>
+      
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+</html>                                                                                                                                                                                                                                                                                                                                   
